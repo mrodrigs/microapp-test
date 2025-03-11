88f14891ef94c9b1d5d72b49b48178488757e548
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2025-03-11 12:16:18 -0300

    install auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,18 @@
+"use client";
+
+import { Auth } from "@microapp-io/auth";
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    const auth = new Auth();
+    const a = async () => {
+      auth.requestLogin();
+    };
+
+    a();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen text-4xl font-bold">
       Hello, Microapp!
