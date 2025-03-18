98ed86b5bbd35aaa3372861ec269a00d03a04859
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2025-03-18 11:26:26 -0300

    Bring auth back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-// import { Auth } from "@microapp-io/auth";
-// import { useEffect } from "react";
+import { Auth } from "@microapp-io/auth";
+import { useEffect } from "react";
 
 export default function Home() {
-  // useEffect(() => {
-  //   const auth = new Auth();
-  //   const a = async () => {
-  //     auth.requestLogin();
-  //   };
+  useEffect(() => {
+    const auth = new Auth();
+    const a = async () => {
+      auth.requestLogin();
+    };
 
-  //   a();
-  // }, []);
+    a();
+  }, []);
 
   return (
     <div className="flex items-center justify-center min-h-screen text-4xl font-bold">
