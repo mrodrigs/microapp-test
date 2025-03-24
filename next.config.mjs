2c64c454e9d248453c607ac203af5cefeec59f16
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2025-03-24 09:54:47 -0300

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import { MicroappNextFederationPlugin } from '@microapp-io/scripts';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+  distDir: '.next',
   webpack: (config, options) => {
     if (!options.isServer) {
       config.plugins.push(new MicroappNextFederationPlugin());
