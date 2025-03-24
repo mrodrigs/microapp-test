84d260de93c246e5a5004a921a66fd033c73b9ac
Mauricio Rodrigues <mrodrigues.professional@gmail.com>
2025-03-24 10:18:15 -0300

    Remove export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import { MicroappNextFederationPlugin } from '@microapp-io/scripts';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   distDir: '.next',
   webpack: (config, options) => {
     if (!options.isServer) {
